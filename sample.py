5e1665c91d84dd17b2c122c9f5bafd09b850f84b
sreekanth <sreekanthj009@gmail.com>
2017-09-14 16:04:26 +0530

    copy

--- sample.py
+++ sample.py
@@ -1027,3 +1027,40 @@
 #     print ('Your original input was', targetOuput)
 # else:
 #     print("Invalid number")
+
+
+#### same prob, multiple inputs and outputs with target base
+
+#     current_input_list = []
+#     current_individual_input = []
+
+#     while True:
+#         current_input = raw_input("Enter base, input and targetBase: ")
+#         if current_input == "":
+#             break
+#         else:
+#             current_input_list.append(current_input)
+
+#     for element in current_input_list:
+#         parts = element.split(',')
+
+#         the_input_dict = {
+#             'base': int(parts[0]),
+#             'input': parts[1],
+#             'targetBase': int(parts[2])
+#         }
+#         current_individual_input.append(the_input_dict)
+
+
+#     for i in current_individual_input:
+#         print ('processing input', i['input'])
+#         isValid = validateInput(i['base'], i['input'])
+#         if isValid:
+#             print("Valid number")
+#             decimalOutput = convertNumber(i['base'], i['input'], i['targetBase'])
+#             print ('The Decimal value of the input is', decimalOutput)
+#             targetOuput = deciToTarget(i['targetBase'], decimalOutput)
+#             print ('The output of target base', targetOuput)
+#         else:
+#             print("Invalid number")
+
